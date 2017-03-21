c3f968e77f0ed7fac8d062122eb51dca5c3cfa8a
BenX <onspeedxxzero@gmail.com>
2017-03-21 23:01:33 +0700

    Update Missing And Wrong File.

---
+++ app/src/main/java/object/dto/timeeventdto.java
@@ -0,0 +1,26 @@
+package object.dto;
+
+import java.text.SimpleDateFormat;
+
+/**
+ * Created by BenX on 18/03/2017.
+ */
+public class timeeventdto {
+    public timeeventdto()
+    {
+        this.id =0;
+        this.userid = 0;
+        this.timestart = new SimpleDateFormat();
+        this.timeend = new SimpleDateFormat();
+        this.note = "";
+        this.pe_id = 0;
+    }
+
+    public int id;
+    public int userid;
+    public SimpleDateFormat timestart;
+    public SimpleDateFormat timeend;
+    public SimpleDateFormat dayselect;
+    public String note;
+    public int pe_id;
+}
