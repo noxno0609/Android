36eab5e6d78db82da2ab83ab4b100998e89ef101
THIEN <phamhongthien.10396@gmail.com>
2017-03-27 22:39:13 +0700

    Update Code Database - Post_insert

--- app/src/main/java/object/database.java
+++ app/src/main/java/object/database.java
@@ -1,14 +1,26 @@
 package object;
 
 import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by BenX on 18/03/2017.
@@ -74,6 +86,43 @@ public class database {
         return result.toString();
     }
 
+    public static String postMethod_insert(EditText a, EditText b, EditText c, EditText d)
+    {
+        //tạo URL dẫn tới Database
+        String url = "http://" + database.connIP + "/TimeReminderWS/public/TimeEvent";
+
+        //khởi tạo giao thức http phái Client
+        HttpClient httpClient = new DefaultHttpClient();
+
+        //Giao thức Post bằng việc lấy URL để nhận Request
+        HttpPost httpPost = new HttpPost(url);
+
+        //Các tham số cần truyền
+        List nameValuePair = new ArrayList(4);
+        nameValuePair.add(new BasicNameValuePair("timestart",a.getText().toString()));
+        nameValuePair.add(new BasicNameValuePair("timeend",b.getText().toString()));
+        nameValuePair.add(new BasicNameValuePair("dayselect",c.getText().toString()));
+        nameValuePair.add(new BasicNameValuePair("note",d.getText().toString()));
+
+        //Encoding dữ liệu khi POST
+        try {
+            httpPost.setEntity(new UrlEncodedFormEntity(nameValuePair));
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
+        String result = "";
+        try {
+            HttpResponse response = httpClient.execute(httpPost);
+            HttpEntity entity = response.getEntity();
+            result = EntityUtils.toString(entity);
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
      /*
     URL weburl = new URL(url);
     URLConnection urlConnection = weburl.openConnection();
