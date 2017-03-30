3921c6229d70be12d567088390f72cc0abbb838d
tongdangkhoa <tongdangkhoa@outlook.com>
2017-03-30 12:52:03 +0700

    update moc

---
+++ app/src/main/java/com/example/thienpham/time_remider/Moc.java
@@ -0,0 +1,95 @@
+package com.example.thienpham.time_remider;
+
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+import java.util.Calendar;
+
+public class Moc extends AppCompatActivity {
+    Button btTimestart,btTimeend,btDayselect,btCancel;
+    Calendar cal = Calendar.getInstance();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_moc);
+        btCancel=(Button) findViewById(R.id.btCancel);
+        btTimestart =(Button) findViewById(R.id.btTimestart);
+        btTimeend =(Button) findViewById(R.id.btTimeend);
+        btDayselect =(Button) findViewById(R.id.btDayselect);
+        btTimestart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                timestart();
+            }
+        });
+        btTimeend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                timeend();
+            }
+        });
+        btDayselect.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public  void  onClick(View v){
+                dayselect();
+            }
+        });
+        btCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Moc.this,ShowActivity.class);
+                startActivity(intent);
+                Toast.makeText(Moc.this, "Bạn đã hủy Mốc", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+    void timestart()
+    {
+        TimePickerDialog tpd = new TimePickerDialog(this, new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                btTimestart.setText(String.format("%02d", hourOfDay)+":"+String.format("%02d", minute));
+            }
+        },
+                cal.get(Calendar.HOUR_OF_DAY),
+                cal.get(Calendar.MINUTE),
+                true);
+
+        tpd.show();
+    }
+    void timeend()
+    {
+        TimePickerDialog tpd = new TimePickerDialog(this, new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                btTimeend.setText(String.format("%02d", hourOfDay)+":"+String.format("%02d", minute));
+            }
+        },
+                cal.get(Calendar.HOUR_OF_DAY),
+                cal.get(Calendar.MINUTE),
+                true);
+
+        tpd.show();
+    }
+    void dayselect()
+    {
+        DatePickerDialog dpd = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                btDayselect.setText(dayOfMonth+"/"+String.format("%02d",month+1)+"/"+year);
+            }
+        },
+                cal.get(Calendar.YEAR),
+                cal.get(Calendar.MONTH),
+                cal.get(Calendar.DAY_OF_MONTH));
+        dpd.show();
+    }
+}
